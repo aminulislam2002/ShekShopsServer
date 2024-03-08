955a0e459cb69d5211ae56a30e81e9964b46d6c7
aminulislam2002 <aminulislamemon2002@gmail.com>
2024-03-09 02:00:57 +0600

    delete an order

--- index.js
+++ index.js
@@ -65,6 +65,14 @@ async function run() {
       res.send(result);
     });
 
+    // DELETE a order
+    app.delete("/deleteOrder/:id", async (req, res) => {
+      const productId = req.params.id;
+      const query = { _id: new ObjectId(productId) };
+      const result = await ordersCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
